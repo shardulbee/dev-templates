663ec7b9f4e0b36f7958384967b780118eb9524b
Shardul Baral <shardul@baral.ca>
2024-05-02 08:55:21 -0400

    fallback to default dev templates

--- flake.nix
+++ flake.nix
@@ -1,8 +1,9 @@
 {
   description = "Ready-made templates for easily creating flake-driven environments";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.default-dev-templates.url = "github:the-nix-way/dev-templates";
 
-  outputs = { self, nixpkgs }: let
+  outputs = { self, nixpkgs, default-dev-templates }: let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
         pkgs = import nixpkgs { inherit system; };
@@ -17,10 +18,10 @@
       inherit (pkgs);
     });
 
-    templates = {
-      python = {
-        path = ./python;
-        description = "A simple python project";
+    templates = default-dev-templates.templates // {
+      ml = {
+        path = ./ml;
+        description = "ML development environment";
       };
     };
   };
